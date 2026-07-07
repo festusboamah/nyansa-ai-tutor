48e0b80a8d3439fd89a0072968fee77635751fa0
Festus Boamah <boamaelikem@gmail.com>
2026-07-07 22:54:53 +0000

    Phase 4 complete: courses admin registered, tested with sample data

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Subject, Material
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("title", "subject", "teacher", "material_type", "uploaded_at")
+    list_filter = ("material_type", "subject")
+    search_fields = ("title",)
