7ef924b2823fb5a8f5e2d121adf676c265e798cf
Festus Boamah <boamaelikem@gmail.com>
2026-07-07 23:51:13 +0000

    Rebrand project as Nyansa

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import StudentSignUpForm
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, "home.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = StudentSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created successfully! Welcome to Nyansa.")
+            return redirect("home")
+    else:
+        form = StudentSignUpForm()
+    return render(request, "accounts/signup.html", {"form": form})
--- config/settings.py
+++ config/settings.py
@@ -81,5 +81,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
