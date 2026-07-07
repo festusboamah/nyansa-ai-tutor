d15c7702fe04f3d50b81dedbeb66f50ce64f8bd7
Festus Boamah <boamaelikem@gmail.com>
2026-07-07 22:26:51 +0000

    Register custom User in Django admin with role field

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "role", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role Info", {"fields": ("role",)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
